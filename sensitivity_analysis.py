7000dac69d599df203d2280b564212377b41bde2
Henry Robbins <hwr26@cornell.edu>
2022-01-18 09:39:52 -0500

    Sample from scenario family in senstivity_analysis script.

--- sensitivity_analysis.py
+++ sensitivity_analysis.py
@@ -4,6 +4,7 @@ import numpy as np
 from groups import population
 import metrics
 from transform import transform
+from scenario import ScenarioFamily
 from sim_helper import sim_test_strategy
 from sp22_strategies import sp22_testing_strategy
 import plotting
@@ -15,19 +16,20 @@ COLORS = ['#084594', '#2171b5', '#4292c6', '#6baed6',
 # [Initialize Parameters]
 # =======================
 
-nominal_scenario = yaml.safe_load(open("nominal.yaml", "r"))
-nominal_scenario["meta_matrix"] = \
-    np.array([list(row.values()) for row in nominal_scenario["meta_matrix"].values()])
+nominal = yaml.safe_load(open("nominal.yaml", "r"))
+nominal["meta_matrix"] = \
+    np.array([list(row.values()) for row in nominal["meta_matrix"].values()])
+# load prior
+scenario_family = ScenarioFamily(nominal, yaml.safe_load(open("prior.yaml", "r")))
 
 # ==========================
 # [Run] Compare trajectories
 # ==========================
 
 trajectories = []
-scalers = np.linspace(0.0,1.0,50)
-for i in range(len(scalers)):
-    scenario = transform(nominal_scenario,
-                         {"booster_rate/FS_multiply_linear_scale": scalers[i]})
+# scalers = np.linspace(0.0,1.0,50)
+for _ in range(100):
+    scenario = scenario_family.get_sampled_scenario()
     traj = sim_test_strategy(scenario=scenario,
                              strategy=sp22_testing_strategy(scenario),
                              color="white",
@@ -38,7 +40,7 @@ for i in range(len(scalers)):
 # [Plot]
 # ======
 
-popul = population.from_scenario(nominal_scenario)
+# popul = population.from_scenario(nominal_scenario)
 plotting.plot_metric_over_time(outfile="sensitivity_analysis.png",
                                 trajectories=trajectories,
                                 metric_name="Cumulative Hospitalizations",
