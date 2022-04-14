1e5cb1a6bf00e082da5639a082fe4ce295790267
Henry Robbins <hwr26@cornell.edu>
2022-04-14 16:52:40 -0400

    Moved scripts into simpar directory with __init__.py

---
+++ simpar/__init__.py
@@ -0,0 +1,20 @@
+"""
+simpar
+======
+
+TODO: Add description
+"""
+
+__author__ = "Cornell Covid Modeling Team"
+
+from . import days_infectious
+from . import groups
+from . import isolation
+from . import metrics
+from . import micro
+from . import plotting
+from . import sim_helper
+from . import sim
+from . import strategy
+from . import trajectory
+from . import transform
