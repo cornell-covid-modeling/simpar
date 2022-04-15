b88acd081d59095272198da462fe2470c8698786
Henry Robbins <hwr26@cornell.edu>
2022-04-15 10:55:02 -0400

    Added dependencies to the setup.py file.

--- setup.py
+++ setup.py
@@ -24,7 +24,12 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=[],
+    install_requires=[
+        "numpy>=1.21",
+        "pandas>=1.2",
+        "matplotlib>=3.3",
+        "scipy>=1.6"
+    ],
     extras_require={
         "dev": []
     },
