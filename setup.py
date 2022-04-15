83c99fd3cb0e07ffe57de6fa1c296b6f57006ddb
Henry Robbins <hwr26@cornell.edu>
2022-04-15 11:16:45 -0400

    Added [dev] extra_requires and updated test_requirements.

--- setup.py
+++ setup.py
@@ -31,7 +31,10 @@ setup(
         "scipy>=1.6"
     ],
     extras_require={
-        "dev": []
+        "dev": ['pytest>=5',
+                'mock>=3',
+                'coverage>=4.5',
+                'tox>=3']
     },
     python_requires='>=3.5',
 )
