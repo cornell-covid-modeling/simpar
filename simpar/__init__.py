28ef93e804b781746bb3e03373b4cd0254a4528b
Henry Robbins <hwr26@cornell.edu>
2022-06-08 00:21:23 -0400

    Bug fix: sim_helper removed from __init__

--- simpar/__init__.py
+++ simpar/__init__.py
@@ -15,7 +15,6 @@ from . import groups
 from . import metrics
 from . import micro
 from . import plotting
-from . import sim_helper
 from . import sim
 from . import strategy
 from . import trajectory
